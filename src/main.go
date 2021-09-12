677fbbe1a0b9742e9907663ef8be2aedea13786b
Alexan123321 <60431697+Alexan123321@users.noreply.github.com>
2021-09-12 11:43:50 +0200

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,3 +1,10 @@
+/**
+BY: Deyana Atanasova, Henrik Tambo Buhl & Alexander Stæhr Johansen 
+DATE: 12-09-2021 (revised)
+COURSE: Distributed Systems and Security
+DESCRIPTION: Distributed chat implemented as structured P2P flooding network.
+**/
+
 package main
 
 import ("ex_2_4/src/packages/peer")

60d702386e479b227cae70f9e35f204804ef87fc
Alexan123321 <60431697+Alexan123321@users.noreply.github.com>
2021-09-15 18:37:48 +0200

    Add files via upload

--- src/main.go
+++ src/main.go
@@ -1,17 +1,12 @@
-/**
-BY: Deyana Atanasova, Henrik Tambo Buhl & Alexander Stæhr Johansen 
-DATE: 12-09-2021 (revised)
-COURSE: Distributed Systems and Security
-DESCRIPTION: Distributed chat implemented as structured P2P flooding network.
-**/
-
 package main
 
-import ("ex_2_4/src/packages/peer")
+import (
+	"after_feedback/src/packages/peer"
+)
 
 func main() {
 	var p = peer.Peer{}
-	p.InitializePeer()
 	p.StartPeer()
-	for true {}
+	for true {
+	}
 }
